331efbf4ae1854b94a7d5d0839cd767232b3652e
Eston Kagwima <ekagwima745@gmail.com>
2021-05-31 00:54:30 +0300

    adding rating path urls

--- tuzo/urls.py
+++ tuzo/urls.py
@@ -13,4 +13,6 @@ urlpatterns = [
   re_path(r'^edit_profile/(?P<userId>\d+)/$', views.edit_profile, name='edit_profile'),
   re_path(r'^profile/(?P<userId>\d+)/$', views.user_profile, name='profile_page'),
   re_path(r'^search/', views.search_results, name='search_results'),
+  re_path(r'^ratings/(?P<project_id>\d+)/$', views.rate, name='rate'),
+
 ]
