9ef27d6ca0c8b05d82f26ee3d5ff272cccc487b1
Eston Kagwima <ekagwima745@gmail.com>
2021-05-30 16:32:36 +0300

    display project information

--- tuzo/views.py
+++ tuzo/views.py
@@ -3,11 +3,13 @@ from django.shortcuts import render,redirect
 from django.http  import HttpResponse,Http404
 import datetime as dt
 from django.contrib.auth.decorators import login_required
+from .models import Project,Profile,Review
 
 # Create your views here.
 
 def landing (request):
+  post =Project.objects.all()
   
 
   title = 'Tuzo-Awards'
-  return render (request,'in',{'title':title,})  
+  return render (request,'index.html',{'title':title,'Posts':post,})  
