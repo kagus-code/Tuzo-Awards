c9c1d2491a7ce56fa57138fdfdfbc23a218dd751
Eston Kagwima <ekagwima745@gmail.com>
2021-05-31 13:33:28 +0300

    installing rest framework and adding serializer

---
+++ tuzo/serializer.py
@@ -0,0 +1,13 @@
+from django.db.models import fields
+from rest_framework import serializers
+from .models import Profile, Project
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('user_name', 'bio', 'profile_pic')
+
+class ProjectSerializers(serializers.ModelSerializer):
+  class Meta:
+    model =Project
+    fields = ('title','lannding_page','description','link','pub_date','publisher')
